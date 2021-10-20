b07babe84cbdd4d43ba3cbac6aff0f124b631605
descomPedro <pedro@descom.es>
2021-10-20 20:06:32 +0200

    validacion front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -405,9 +405,7 @@
                 color: rgba(107, 114, 128, var(--tw-text-opacity))
             }
         }
-    </style>
 
-    <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
@@ -417,12 +415,17 @@
 <body class="antialiased">
     <form action="/dashboard" method="POST" name="form" value="submit">
         <div class="row justify-content-center">
+            @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                Algunas preguntas se quedaron sin responder. Realice de nuevo el cuestionario
+            </div>
+            @endif
             <div id="my-form" class="col-12 col-md-10 col-lg-8 text-center my-form">
                 @csrf
                 <p>1.-Todos somos conscientes de los comportamientos premiados y aceptados por el equipo y actuamos en consecuencia.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option1" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option1" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -445,18 +448,14 @@
                 <!-- <button type="submit" class="btn btn-success" style="vertical-align:middle" id="boton" value="submit">Enviar</button> -->
 
 
-                @if ($errors->any())
-                <div class="alert alert-danger" role="alert">
-                    Seleccione una de las opciones disponibles
-                </div>
-                @endif
+
             </div>
             <div class="col-12 col-md-10 col-lg-8 text-center my-form" id="my-form">
                 @csrf
                 <p>2.-Todos disfrutamos de discutir y compartir los problemas apasionadamente y sin tapujos.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option2" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option2" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -476,18 +475,14 @@
                     <label class="form-check-label" for="inlineRadio5">siempre</label>
                 </div>
 
-                @if ($errors->any())
-                <div class="alert alert-danger" role="alert">
-                    Seleccione una de las opciones disponibles
-                </div>
-                @endif
+
             </div>
             <div class="col-12 col-md-10 col-lg-8 text-center my-form" id="my-form">
                 @csrf
                 <p>3.-Señalamos las deficiencias y conductas improductivas propias y de los demás, sin miedos.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option3" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option3" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -507,18 +502,14 @@
                     <label class="form-check-label" for="inlineRadio5">siempre</label>
                 </div>
 
-                @if ($errors->any())
-                <div class="alert alert-danger" role="alert">
-                    Seleccione una de las opciones disponibles
-                </div>
-                @endif
+
             </div>
             <div class="col-12 col-md-10 col-lg-8 text-center my-form" id="my-form">
                 @csrf
                 <p>4.-Sabemos en qué están trabajando los otros miembros del equipo y cómo contribuyen a los objetivos comunes.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option4" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option4" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -543,7 +534,7 @@
                 <p>5.-Las normas de funcionamiento del equipo son conocidas por todos y apelamos a ellas cuando alguien se las salta.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option5" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option5" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -568,7 +559,7 @@
                 <p>6.-Nos disculpamos en el acto y con toda sinceridad cuando decimos o hacemos algo inadecuado o posiblemente perjudicial para el equipo.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option6" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option6" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -593,7 +584,7 @@
                 <p>7.-Son importantes el respeto, la camaradería y la escucha de los demás, a nivel personal y profesional.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option7" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option7" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -620,7 +611,7 @@
                 <p>8.-Todos los miembros del equipo estamos dispuestos a sacrificar objetivos personales (tiempo, presupuesto, proyecto, carrera... ) por el bien común del equipo.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option8" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option8" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -645,7 +636,7 @@
                 <p>9.-Confesamos abiertamente nuestras debilidades y errores, para mejora de todos.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option9" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option9" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -670,7 +661,7 @@
                 <p>10.-Las reuniones del equipo son apasionantes, abiertas, enriquecedoras... nunca aburridas.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option10" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option10" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -695,7 +686,7 @@
                 <p>11.-Nos marchamos de las reuniones confiados en que nuestros compañeros están por completo comprometidos con las decisiones acordadas, aunque hubiera desacuerdos inicialmente.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option11" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option11" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -720,7 +711,7 @@
                 <p>12.-En este equipo, el apoyo emocional a compañeros de trabajo en momentos difíciles está asegurado.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option12" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option12" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -745,7 +736,7 @@
                 <p>13.-El equipo lo pasa realmente mal cuando no se logran los objetivos comunes.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option13" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option13" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -770,7 +761,7 @@
                 <p>14.-Durante las reuniones del equipo, los asuntos más importantes y difíciles se ponen sobre la mesa para ser resueltos.</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option14" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option14" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -795,7 +786,7 @@
                 <p>15.-Hay unas reglas claras de funcionamiento en el equipo (valores compartidos, creencias, lo que es importante, lo que es accesorio...)</p>
 
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option15" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option15" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -819,7 +810,7 @@
                 @csrf
                 <p>16.-A los miembros de este equipo nos preocupa seriamente la perspectiva de defraudar a nuestros compañeros.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option16" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option16" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -843,7 +834,7 @@
                 @csrf
                 <p>17.-Confiamos en que los errores son aceptados para mejorar y no son ocultados a los demás miembros del equipo.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option17" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option17" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -867,7 +858,7 @@
                 @csrf
                 <p>18.-Terminamos nuestros debates y reuniones con resoluciones claras, específicas y con un plan de acción.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option18" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option18" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -891,7 +882,7 @@
                 @csrf
                 <p>19.-Sabemos que la vida personal de cada uno afecta al equipo y lo tenemos en cuenta para ayudarnos.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option19" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option19" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -915,7 +906,7 @@
                 @csrf
                 <p>20.-Nos desafiamos unos a otros acerca de nuestros proyectos, ideas nuevas y planteamientos.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option20" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option20" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -939,7 +930,7 @@
                 @csrf
                 <p>21.-Los miembros de nuestro equipo no tienen prisa en destacar sus propias contribuciones pero señalan las de los demás sin pérdida de tiempo.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option21" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option21" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -963,7 +954,7 @@
                 @csrf
                 <p>22.-Sabemos lo que hay que hacer, cómo, cuando y quien lo tiene que hacer.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option22" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option22" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -987,7 +978,7 @@
                 @csrf
                 <p>23.-La Confianza es uno de nuestros pilares.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option23" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option23" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1011,7 +1002,7 @@
                 @csrf
                 <p>24.-Gestionamos los conflictos de manera positiva para ayudarnos unos a otros, promoviendo el aprendizaje conjunto.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option24" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option24" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1036,7 +1027,7 @@
                 @csrf
                 <p>25.-Estamos totalmente comprometidos con los objetivos comunes.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option25" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option25" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1060,7 +1051,7 @@
                 @csrf
                 <p>26.-Estamos todos "en el mismo barco" y nos co-responsabilizamos de "llegar a buen puerto" juntos.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option26" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option26" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1084,7 +1075,7 @@
                 @csrf
                 <p>27.-Conseguir los objetivos nos llena de satisfacción.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option27" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option27" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1108,7 +1099,7 @@
                 @csrf
                 <p>28.-La comunicación, abierta y sincera, es importante en nuestro equipo.</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="option28" id="inlineRadio1" value="1">
+                    <input class="form-check-input" type="radio" name="option28" id="inlineRadio1" value="1" required>
                     <label class="form-check-label" for="inlineRadio1">nunca</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -1129,7 +1120,7 @@
                 </div>
                 @if ($errors->any())
                 <div class="alert alert-danger" role="alert">
-                    Seleccione una de las opciones disponibles
+                    Algunas preguntas se quedaron sin responder. Realice de nuevo el cuestionario
                 </div>
                 @endif
             </div>
