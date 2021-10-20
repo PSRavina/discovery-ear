0aea25857c7fab1c28dbb5a1c25bb1717ac15b90
descomPedro <pedro@descom.es>
2021-10-20 18:38:09 +0200

    add favicon all pages

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -3,6 +3,7 @@
 <head>
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="shortcat icon" href="favicon.ico">
 </head>
 <style>
     body {
