4e92ca6b75e3347444e7ecc3a9ed1badee2f6ed3
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-02-28 22:01:20 +0100

    add heading xport

--- app/Exports/RequestFormulary.php
+++ app/Exports/RequestFormulary.php
@@ -4,13 +4,57 @@ namespace App\Exports;
 
 use App\Models\Survey;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class RequestFormulary implements FromCollection
+class RequestFormulary implements FromCollection, WithHeadings, WithTitle
 {
     /**
      * @return \Illuminate\Support\Collection
      */
+
+    public function title(): string
+    {
+        return 'Reporte Completo del Equipo';
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Fecha',
+            'Nombre',
+            'Email',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15',
+            '16',
+            '17',
+            '18',
+            '19',
+            '20',
+            '21',
+            '22',
+            '23',
+            '24',
+            '25',
+            '26',
+            '27',
+            '28'
+        ];
+    }
     public function collection()
     {
         $rows = DB::connection()
