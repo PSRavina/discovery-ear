b9a56b35d52f8b4b97647abd0758308aa2a1eba7
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-03-29 22:48:00 +0200

    fix sql

--- app/Exports/AverageItems.php
+++ app/Exports/AverageItems.php
@@ -29,20 +29,19 @@ class AverageItems implements FromCollection, WithHeadings, WithTitle
             'Escucha Integrativa',
         ];
     }
-
+    
     public function collection()
     {
         $rows = DB::connection()
             ->table('surveys as srv')
             ->selectRaw(
-                '((option2 + option4+ option6+option10+option15+option19)*10)/6 Escucha Previa,
-                ((option1 +option3+option7+option11+option16+option22)*10)/6 Escucha Enfocada,
-                ((option8 +option12+option18+option20+option23+option13)*10)/6 Escucha Global,
-                ((option5 +option9+option14+option24+option17+option21)*10)/6 Escucha Integrativa,
-            '
-            )
+                '(((option2) + (option4)+ (option6)+(option10)+(option15)+(option19))*10)/6 Escucha Previa,
+                (((option1) +(option3)+(option7)+(option11)+(option16)+(option22))*10)/6 Escucha Enfocada,
+                (((option8) +(option12)+(option18)+(option20)+(option23)+(option13))*10)/6 Escucha Global,
+                (((option5) +(option9)+(option14)+(option24)+(option17)+(option21))*10)/6 Escucha Integrativa,
+            ')
             ->join('users as usr', 'usr.id', '=', 'srv.user_id')
             ->get();
-        return $rows;
+            return $rows;
     }
 }
