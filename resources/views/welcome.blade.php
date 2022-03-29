0f9f9889a4fde5bbf267608006a23224da4ffdda
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-03-29 21:53:54 +0200

    fix blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -819,8 +819,6 @@
                         <input class="form-check-input" type="radio" name="option24" id="inlineRadio1" value="1" required>
                         <label class="form-check-label" for="inlineRadio1">Sí</label>
                     </div>
-
-
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="option24" id="inlineRadio5" value="0">
                         <label class="form-check-label" for="inlineRadio5">No</label>
