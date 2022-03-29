95a6ce35d172c66dbe3647a3499d6b89c263d4da
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-03-29 22:49:30 +0200

    fix sql

--- app/Exports/AverageItems.php
+++ app/Exports/AverageItems.php
@@ -35,10 +35,10 @@ class AverageItems implements FromCollection, WithHeadings, WithTitle
         $rows = DB::connection()
             ->table('surveys as srv')
             ->selectRaw(
-                '(((option2) + (option4)+ (option6)+(option10)+(option15)+(option19))*10)/6 Escucha Previa,
-                (((option1) +(option3)+(option7)+(option11)+(option16)+(option22))*10)/6 Escucha Enfocada,
-                (((option8) +(option12)+(option18)+(option20)+(option23)+(option13))*10)/6 Escucha Global,
-                (((option5) +(option9)+(option14)+(option24)+(option17)+(option21))*10)/6 Escucha Integrativa,
+                '((option2) + (option4)+ (option6)+(option10)+(option15)+(option19))*10 Escucha Previa,
+                ((option1) +(option3)+(option7)+(option11)+(option16)+(option22))*10Escucha Enfocada,
+                ((option8) +(option12)+(option18)+(option20)+(option23)+(option13))*10 Escucha Global,
+                ((option5) +(option9)+(option14)+(option24)+(option17)+(option21))*10 Escucha Integrativa,
             ')
             ->join('users as usr', 'usr.id', '=', 'srv.user_id')
             ->get();
