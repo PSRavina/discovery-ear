3e76025a05d18de21d2d73234499b2196e7dcf4c
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-03-29 22:37:13 +0200

    add items por diferentes tipos de escucha

--- app/Exports/AverageItems.php
+++ app/Exports/AverageItems.php
@@ -17,19 +17,16 @@ class AverageItems implements FromCollection, WithHeadings, WithTitle
      */
     public function title(): string
     {
-        return 'Resultados del grupo por bloques';
+        return 'Resultados en base 10 por tipo de Escucha';
     }
 
     public function headings(): array
     {
         return [
-            'C.Común',
-            'Confianza',
-            'GP.Conflictos',
-            'Compromiso',
-            'Corresponsabilidad',
-            'O.Resultados',
-            'O.Relaciones'
+            'Escucha Previa',
+            'Escucha Enfocada',
+            'Escucha Global',
+            'Escucha Integrativa',
         ];
     }
     
@@ -38,14 +35,11 @@ class AverageItems implements FromCollection, WithHeadings, WithTitle
         $rows = DB::connection()
             ->table('surveys as srv')
             ->selectRaw(
-                '(avg(option1) + avg(option5)+ avg(option15)+ avg(option22))/4 Ccomun,
-                (avg(option6) + avg(option9)+ avg(option17)+ avg(option23))/4 Confianza,
-                (avg(option2) + avg(option10)+ avg(option14)+ avg(option24))/4 Gconflictos,
-                (avg(option4) + avg(option11)+ avg(option18)+ avg(option25))/4 Compromiso,
-                (avg(option3) + avg(option16)+ avg(option20)+ avg(option26))/4 Corresponsabilidad,
-                (avg(option8) + avg(option13)+ avg(option21)+ avg(option27))/4 Oresultados,
-                (avg(option7) + avg(option12)+ avg(option19)+ avg(option28))/4 Orelaciones'
-            )
+                '(option2) + (option4)+ (option6)+(option10)+(option15)+(option19) Escucha Previa,
+                (option1) +(option3)+(option7)+(option11)+(option16)+(option22) Escucha Enfocada,
+                (option8) +(option12)+(option18)+(option20)+(option23)+(option13) Escucha Global,
+                (option5) +(option9)+(option14)+(option24)+(option17)+(option21) Escucha Integrativa,
+            ')
             ->join('users as usr', 'usr.id', '=', 'srv.user_id')
             ->get();
             return $rows;
