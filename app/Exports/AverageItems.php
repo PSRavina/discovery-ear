c26938c641a44b01e8557b9e077b80cbffec724f
Pedro Sanchez <pedrosanchez@MacBook-Pro-de-Descom.local>
2022-04-20 20:22:52 +0200

    fix averageitems with escuchas types

--- app/Exports/AverageItems.php
+++ app/Exports/AverageItems.php
@@ -34,11 +34,14 @@ class AverageItems implements FromCollection, WithHeadings, WithTitle
     {
         $rows = DB::connection()
             ->table('surveys as srv')
-            ->select(
-                '(option2 + option4 + option6 + option10 + option15 + option19),(option1 + option3 + option7 + option11 + option16 + option22),(option8 + option12+ option18+ option20+ option23+ option13),(option5 + option9 + option14 + option24 + option17 + option21)'
+            ->selectRaw(
+                '(option2 + option4 + option6 + option10 + option15 + option19)*1.66680,
+                (option1 + option3 + option7 + option11 + option16 + option22)*1.66680,
+                (option8 + option12+ option18+ option20+ option23+ option13)*1.66680,
+                (option5 + option9 + option14 + option24 + option17 + option21)*1.66680'
             )
             ->join('users as usr', 'usr.id', '=', 'srv.user_id')
-            ->dd();
+            ->get();
         return $rows;
     }
 }
